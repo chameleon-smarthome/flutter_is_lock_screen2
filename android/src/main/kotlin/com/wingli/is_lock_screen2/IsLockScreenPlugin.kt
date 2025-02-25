b88397cc333ba856be5643215432fb39fffba0eb
Szalók Dániel <daniel.szalok@chameleon.sh>
2025-02-25 13:03:22 +0100

    .

--- android/src/main/kotlin/com/wingli/is_lock_screen2/IsLockScreenPlugin.kt
+++ android/src/main/kotlin/com/wingli/is_lock_screen2/IsLockScreenPlugin.kt
@@ -28,23 +28,6 @@ public class IsLockScreenPlugin(val registrarContext: Context? = null) : Flutter
     channel.setMethodCallHandler(this)
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "is_lock_screen")
-      channel.setMethodCallHandler(IsLockScreenPlugin(registrarContext = registrar.activeContext()))
-    }
-  }
-
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     when (call.method) {
       "isLockScreen" -> {
